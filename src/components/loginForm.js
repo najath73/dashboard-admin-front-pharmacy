db034c3743f466fb5d82ee9bab219697624f9ae1
Imrane Lawani <naimlawani01@gmail.com>
2024-09-12 00:08:46 +0200

    feat: error management

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useAuth } from '../hooks/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { Container, TextField, Button, Typography, Grid, Box, Alert, Paper } from '@mui/material';
+import { AuthorizationError } from '../utils/errors';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
@@ -18,7 +19,12 @@ const LoginPage = () => {
       navigate('/dashboard-admin'); // Redirection après login
     } catch (error) {
       console.error('Erreur lors de la connexion:', error);
-      setError("Nom d'utilisateur ou mot de passe incorrect"); // Mettre à jour l'état d'erreur
+      // Gérer les erreurs en fonction de leur type
+      if (error instanceof AuthorizationError) {
+        setError(error.message);
+      } else {
+        setError(error.message || "Nom d'utilisateur ou mot de passe incorrect.");
+      }
     }
   };
 
